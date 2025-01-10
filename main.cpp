7fc31ae344cbc226e1325495dbce02988581b84b
Ismael <igallomunoz@proton.me>
2025-01-10 14:05:49 +0100

    menu sin funcion

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <stdlib.h>
+#include <iostream>
 #include <stdio.h>
 #include <ncurses.h>
 
 #define ALTO 15
 #define ANCHO 25
 
-int main() {
+int main()
+{
     initscr();
     raw();
     cbreak();
@@ -13,49 +15,31 @@ int main() {
     keypad(stdscr, TRUE);
     curs_set(0);
 
-    // Imprimir el tamaño detectado de la terminal
-    printf("LINES: %d, COLS: %d\n", LINES, COLS);
-    refresh();
+    int start_y = (LINES - ALTO) / 2;
+    int start_x = (COLS - ANCHO) / 2;
 
-    // Calcula las coordenadas para centrar la ventana
-    int start_y = (LINES - ALTO) / 2;        // Coordenada y centrada
-    int start_x = (COLS - ANCHO) / 2;    // Coordenada x centrada
-
-    // Crear la ventana
     WINDOW *win = newwin(ALTO, ANCHO, start_y, start_x);
-    box(win, 0, 0);  // Dibuja un cuadro en la ventana
-    mvwprintw(win, 0, (ANCHO-16)/2, " MANUELBOT v0.1 ");
-    wrefresh(win); // Actualiza la ventana
+    box(win, 0, 0);
+    mvwprintw(win, 0, (ANCHO - 16) / 2, " MANUELBOT v0.1 ");
+    refresh();
+    wrefresh(win);
 
     // Opciones
-    char* opciones[] = { "Conectar", "Apagar" };
+    const char* opciones[] = {"Conectar", "Apagar"};
 
     // Menu
     int opcion = 0;
     bool fin = false;
-    while (!fin) {
-        wclear(win);
-        box(win, 0, 0);
-        mvwprintw(win, 0, (ANCHO-16)/2, " MANUELBOT v0.1 ");
-        wrefresh(win);
-        wrefresh(win);
-        int c = wgetch(win);
-        if (c == KEY_UP) {
-            if (opcion > 0) {
-                opcion--;
-            }
-        } else if (c == KEY_DOWN) {
-            if (opcion < 1) {
-                opcion++;
-            }
-        } else if (c == KEY_ENTER) {
-            break;
-        }
+
+    // Imprime las opciones
+    for (int i = 0; i < 2; i++)
+    {
+        mvwprintw(win, 2*i+2, 2, opciones[i]);
     }
+    wrefresh(win);
+    int tecla = getch();
 
-    getch();
-
-    // Finaliza ncurses
-    endwin();
-    return 0;
+// Finaliza ncurses
+endwin();
+return 0;
 }
